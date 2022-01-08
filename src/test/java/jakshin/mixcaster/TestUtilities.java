fada217101fb202e37fcfc41d1257d25980c0cb6
Jason Jackson <jasonjackson@pobox.com>
2022-01-07 21:53:49 -0700

    Add a method for mocking up a music directory

--- src/test/java/jakshin/mixcaster/TestUtilities.java
+++ src/test/java/jakshin/mixcaster/TestUtilities.java
@@ -17,9 +17,14 @@
 
 package jakshin.mixcaster;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.fail;
@@ -28,6 +33,33 @@ import static org.junit.jupiter.api.Assertions.fail;
  * Utility functions for use in tests.
  */
 public class TestUtilities {
+    /**
+     * Creates a temporary directory with some fake files in it,
+     * and puts its path into the music_dir system property.
+     */
+    @NotNull
+    public static Path createMockMusicDir(String prefix) throws IOException {
+        Path mockMusicDir = Files.createTempDirectory(prefix);
+        Files.createDirectory(Path.of(mockMusicDir.toString(), "dir"));
+
+        Path subdir = Path.of(mockMusicDir.toString(), "dir/subdir");
+        Files.createDirectory(subdir);
+
+        Path file = Path.of(mockMusicDir.toString(), "dir/file.m4a");
+        Files.writeString(file, "fake file data");
+        Files.setLastModifiedTime(file, FileTime.from(Instant.now().minus(5, ChronoUnit.MINUTES)));
+
+        Files.createSymbolicLink(Path.of(mockMusicDir.toString(), "dir/file-link"), file);
+        Files.createSymbolicLink(Path.of(mockMusicDir.toString(), "dir/subdir-link"), subdir);
+
+        System.setProperty("music_dir", mockMusicDir.toString());
+        System.setProperty("http_cache_time_seconds", "3600");
+        System.setProperty("episode_max_count", "25");
+        System.setProperty("download_threads", "auto");
+
+        return mockMusicDir;
+    }
+
     /**
      * Recursively removes a temporary directory.
      * Intended for use in afterAll/tearDown methods.
@@ -38,12 +70,12 @@ public class TestUtilities {
             return;
         }
 
-        String systemTempDir = System.getProperty("java.io.tmpdir");
-        if (! tempDir.toString().startsWith(systemTempDir)) {
-            fail("That's not a temp directory! " + tempDir);
-        }
-
         try {
+            String systemTempDir = Path.of(System.getProperty("java.io.tmpdir")).toRealPath().toString();
+            if (! tempDir.toRealPath().toString().startsWith(systemTempDir)) {
+                fail("That's not a temp directory! " + tempDir);
+            }
+
             Files.walk(tempDir).sorted(Comparator.reverseOrder()).forEach(path -> {
                 try {
                     Files.delete(path);
